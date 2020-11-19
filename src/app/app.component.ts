ae6f93a17b04254ea88cf272af568ba0db4588bc
Simo Ngquseka <simo@swssr.co>
2020-11-19 23:13:19 +0200

    Add restaurant list and coming soon banner for customize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { API_URL } from 'src/config';
 import { Meal } from './interfaces';
+import { Restaurant } from './interfaces/restaurant';
 
 @Component({
   selector: 'app-root',
@@ -14,18 +16,32 @@ export class AppComponent implements OnInit {
   activeTab: string = this.tabs[1];
 
   menu: Meal[] = null;
+  restaurants: Restaurant[] = null;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getMenu();
+    this.getRestaurants();
   }
 
   async getMenu() {
-    this.http.get('http://localhost:5000/menu').subscribe((res: any) => {
+    this.http.get(`${API_URL}/menu`).subscribe((res: any) => {
       if (!res) return;
       this.menu = res;
     });
+  }
+
+  async getRestaurants() {
+    this.http
+      .get<{ restaurants: Restaurant[] } | null>(
+        'https://tablefor.ew.r.appspot.com/restaurant/list'
+      )
+      .subscribe((res) => {
+        if (!res) return;
+        this.restaurants = res?.restaurants;
+        console.log(this.restaurants);
+      });
   }
 
   setTab(tab: string) {
