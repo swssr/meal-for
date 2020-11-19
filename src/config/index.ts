5be1c4e537cfebb3f8e9b92075d4c75165cb9377
Simo Ngquseka <simo@swssr.co>
2020-11-20 00:54:01 +0200

    clean up api url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
-export const API_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5000'
-    : 'https://meal-for.nw.r.appspot.com';
+// export const API_URL =
+//   process.env.NODE_ENV === 'development'
+//     ? 'http://localhost:5000'
+//     : 'https://meal-for.nw.r.appspot.com';
+export const API_URL = 'https://meal-for.nw.r.appspot.com';
