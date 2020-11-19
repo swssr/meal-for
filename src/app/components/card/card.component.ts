8bdea145f9b2809d7f99d83203b9678b3ed3c04a
Simo Ngquseka <simo@swssr.co>
2020-11-19 20:05:15 +0200

    Add fancy UI

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { Meal } from 'src/app/interfaces';
 
@@ -8,7 +9,33 @@ import { Meal } from 'src/app/interfaces';
 })
 export class CardComponent {
   @Input() meal: Meal;
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
+
+  handleBuy(_meal: Meal) {
+    const data = {
+      SiteCode: 'L5BBE^dVnedQDnTFfzex6_rpHWTDD#npcK4Zqv*e',
+      CountryCode: 'SA',
+      CurrencyCode: 'ZAR',
+      Amount: _meal.amount,
+      TransactionReference: 'dVnedQDnTFfzex6_rpHWTDD#npcK4Zqv*e',
+      IsTest: true,
+      HashCheck:
+        'VnedQDnTFfzex6_rpHWTDD#npcK4Zqv*eBE^dVnedQDnTFfzex6_rpHWTDD#npcK4Zqv*e',
+      NotifyUrl: 'https://api.tablefor.app/confirm_payment',
+    };
+
+    const options = {};
+
+    this.http
+      .post('https://pay.ozow.com', data, options)
+      .toPromise()
+      .then((value) => {
+        console.log({ value });
+      })
+      .catch((err) => {
+        console.log({ err });
+      });
+  }
 }
--- src/app/interfaces/meal.ts
+++ src/app/interfaces/meal.ts
@@ -2,4 +2,5 @@ export interface Meal {
   name: string;
   img: string;
   description: string;
+  amount?: number;
 }
